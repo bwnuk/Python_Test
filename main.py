9e992048292b4ab36563107682fbe97dac376599
Bartłomiej Wnuk <bartekwnuk13@gmail.com>
2018-04-27 20:21:15 +0200

    Class and button.

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from tkinter import *
 
+
+
+class MButtons:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.printButton = Button(frame, text="Print", command=self.printMessage)
+        self.printButton.pack(side=LEFT)
+
+        self.quitButton = Button(frame, text="Quit", command=frame.quit)
+        self.quitButton.pack(side=LEFT)
+
+    def printMessage(self):
+        print("WOW")
+
 root = Tk()
-
-def leftClick(event):
-    print("Left")
-
-def middleClick(event):
-    print("Middle")
-
-def rightClick(event):
-    print("Right")
-
-frame = Frame(root, width=300, height=250)
-frame.bind("<Button-1>", leftClick)
-frame.bind("<Button-2>", middleClick)
-frame.bind("<Button-3>", rightClick)
-frame.pack()
+b = MButtons(root)
 
 root.mainloop()
