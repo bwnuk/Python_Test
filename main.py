29d5f8b5c5ccb338f6c1ddf7a64046923c108768
Bartłomiej Wnuk <bartekwnuk13@gmail.com>
2018-04-27 20:56:52 +0200

    Toolbar, Status bar added.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def doNothing():
 
 root = Tk()
 
+# *** MENU ***
+
 menu_m = Menu(root)
 root.config(menu=menu_m)
 
@@ -18,5 +20,22 @@ subMenu.add_command(label="Exit", command=doNothing)
 editMenu = Menu(menu_m)
 menu_m.add_cascade(label="Edit", menu = editMenu)
 editMenu.add_command(label="New..", command=doNothing)
+
+# *** TOOLBAR ***
+
+toolbar = Frame(root, bg="blue")
+
+insertButton = Button(toolbar, text="Insert image", command=doNothing)
+insertButton.pack(side=LEFT, padx=2, pady=2)
+
+printButton = Button(toolbar, text="Print", command=doNothing)
+printButton.pack(side=LEFT, padx=2, pady=2)
+
+toolbar.pack(side=TOP, fill=X)
+
+# *** Status Bar ***
+
+status = Label(root, text="Doing nothing", bd=1, relief=SUNKEN, anchor=W)
+status.pack(side=BOTTOM, fill=X)
 
 root.mainloop()
