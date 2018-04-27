a33e74630939c4b12c08b1779f5318c7797bd977
Bartłomiej Wnuk <bartekwnuk13@gmail.com>
2018-04-27 20:28:41 +0200

    Simple menu.

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from tkinter import *
 
-
-
-class MButtons:
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
-
-        self.printButton = Button(frame, text="Print", command=self.printMessage)
-        self.printButton.pack(side=LEFT)
-
-        self.quitButton = Button(frame, text="Quit", command=frame.quit)
-        self.quitButton.pack(side=LEFT)
-
-    def printMessage(self):
-        print("WOW")
+def doNothing():
+    print("OK")
 
 root = Tk()
-b = MButtons(root)
+
+menu_m = Menu(root)
+root.config(menu=menu_m)
+
+subMenu = Menu(menu_m)
+menu_m.add_cascade(label="File", menu = subMenu)
+subMenu.add_command(label="New Project", command=doNothing)
+subMenu.add_command(label="New..", command=doNothing)
+subMenu.add_separator()
+subMenu.add_command(label="Exit", command=doNothing)
+
+editMenu = Menu(menu_m)
+menu_m.add_cascade(label="Edit", menu = editMenu)
+editMenu.add_command(label="New..", command=doNothing)
 
 root.mainloop()
